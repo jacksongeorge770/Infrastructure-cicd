fd2cb8fc345a8ecfc051ddcf6327e91f10cd3e15
jacksongeorge770 <jacksongeorge770@gmail.com>
2025-05-18 03:29:54 -0400

    error fixed

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2"
+    }
+    jenkins = {
+      source  = "taiidani/jenkins" # Correct provider source
+      version = "~> 0.5.0"
+    }
+  }
+}
+
+
+
 provider "aws" {
   region = var.region
 }
