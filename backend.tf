e9191c50b932b6ea428cbc33579e28112da896b9
jacksongeorge770 <jacksongeorge770@gmail.com>
2025-05-19 09:53:50 -0400

    delete s3 and db from terraform

--- backend.tf
+++ backend.tf
@@ -1,45 +1,45 @@
-# Defining the S3 bucket for Terraform state
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "jackson-terraform-ca-central1" # Replace with a unique bucket name
+# # Defining the S3 bucket for Terraform state
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket = "jackson-terraform-ca-central1" # Replace with a unique bucket name
 
-  tags = {
-    Name = "TerraformStateBucket"
-  }
+#   tags = {
+#     Name = "TerraformStateBucket"
+#   }
   
-}
+# }
 
-# Enabling versioning on the S3 bucket
-resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
+# # Enabling versioning on the S3 bucket
+# resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+#   bucket = aws_s3_bucket.terraform_state.id
 
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
 
-# Configuring server-side encryption with AES256
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encryption" {
-  bucket = aws_s3_bucket.terraform_state.id
+# # Configuring server-side encryption with AES256
+# resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encryption" {
+#   bucket = aws_s3_bucket.terraform_state.id
 
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+# }
 
-# Defining the DynamoDB table for state locking
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
+# # Defining the DynamoDB table for state locking
+# resource "aws_dynamodb_table" "terraform_locks" {
+#   name         = "terraform-locks"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "LockID"
 
-  attribute {
-    name = "LockID"
-    type = "S" # String type
-  }
+#   attribute {
+#     name = "LockID"
+#     type = "S" # String type
+#   }
 
-  tags = {
-    Name = "TerraformLockTable"
-  }
-}
+#   tags = {
+#     Name = "TerraformLockTable"
+#   }
+# }
